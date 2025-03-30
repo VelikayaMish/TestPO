5aa771e5a1e8fc55d8969c57cf97a98570d61857
VelikayaMish <afpalsmlo@gmail.com>
2025-03-30 20:32:32 +0300

    function.py

---
+++ function.py
@@ -0,0 +1,14 @@
+import cmath
+
+def solve_quadratic(a, b, c):
+    if a == 0:
+        raise ValueError("Coefficient 'a' cannot be zero.")
+    
+    # Вычисляем дискриминант
+    d = b**2 - 4*a*c
+    
+    # Находим два корня
+    x1 = (-b + cmath.sqrt(D)) / (2 * a)
+    x2 = (-b - cmath.sqrt(D)) / (2 * a)
+    
+    return x1, x2
